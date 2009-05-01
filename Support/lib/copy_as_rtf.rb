52638c9884b4b377ae3dbce1bb12c83e4a8a0f57
Dr Nic Williams <drnicwilliams@gmail.com>
2009-05-01 15:14:17 +1000

    convert tabs to spaces

--- Support/lib/copy_as_rtf.rb
+++ Support/lib/copy_as_rtf.rb
@@ -56,56 +56,56 @@ class RtfExporter
   end
   
   def generate_stylesheet_from_theme(theme_class = nil)
-  	theme_class = '' if theme_class == nil
-  	require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
+    theme_class = '' if theme_class == nil
+    require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
 
-  	# Load TM preferences to discover the current theme and font settings
-  	textmate_pref_file = '~/Library/Preferences/com.macromates.textmate.plist'
-  	prefs = PropertyList.load(File.open(File.expand_path(textmate_pref_file)))
-  	theme_uuid = prefs['OakThemeManagerSelectedTheme']
-  	# Load the active theme. Unfortunately, this requires us to scan through
-  	# all discoverable theme files...
-  	unless theme_plist = find_theme(theme_uuid)
-  		print "Could not locate your theme file!"
-  		abort
-  	end
+    # Load TM preferences to discover the current theme and font settings
+    textmate_pref_file = '~/Library/Preferences/com.macromates.textmate.plist'
+    prefs = PropertyList.load(File.open(File.expand_path(textmate_pref_file)))
+    theme_uuid = prefs['OakThemeManagerSelectedTheme']
+    # Load the active theme. Unfortunately, this requires us to scan through
+    # all discoverable theme files...
+    unless theme_plist = find_theme(theme_uuid)
+      print "Could not locate your theme file!"
+      abort
+    end
 
-  	theme_comment = theme_plist['comment']
-  	theme_name = theme_plist['name']
-  	theme_class.replace(theme_name)
-  	theme_class.downcase!
-  	theme_class.gsub!(/[^a-z0-9_-]/, '_')
-  	theme_class.gsub!(/_+/, '_')
+    theme_comment = theme_plist['comment']
+    theme_name = theme_plist['name']
+    theme_class.replace(theme_name)
+    theme_class.downcase!
+    theme_class.gsub!(/[^a-z0-9_-]/, '_')
+    theme_class.gsub!(/_+/, '_')
 
-  	@font_name = prefs['OakTextViewNormalFontName'] || 'Monaco'
-  	@font_size = (prefs['OakTextViewNormalFontSize'] || 11).to_s
-  	@font_size.sub! /\.\d+$/, ''
-  	@font_size = @font_size.to_i * 3
-  	
-  	@font_name = '"' + @font_name + '"' if @font_name.include?(' ') &&
-  		!@font_name.include?('"')
+    @font_name = prefs['OakTextViewNormalFontName'] || 'Monaco'
+    @font_size = (prefs['OakTextViewNormalFontSize'] || 11).to_s
+    @font_size.sub! /\.\d+$/, ''
+    @font_size = @font_size.to_i * 3
+    
+    @font_name = '"' + @font_name + '"' if @font_name.include?(' ') &&
+      !@font_name.include?('"')
 
-  	theme_plist['settings'].each do | setting |
-  	  if (!setting['name'] and setting['settings'])
-  			body_bg = setting['settings']['background'] || '#ffffff'
-  			body_fg = setting['settings']['foreground'] || '#000000'
-  			selection_bg = setting['settings']['selection']
-  			body_bg = hex_color_to_rtf(body_bg)
-  			body_fg = hex_color_to_rtf(body_fg)
-  			selection_bg = hex_color_to_rtf(selection_bg) if selection_bg
-  			@colors << body_fg
-  			next
-  		end
-  	  if setting['name'] && setting['scope']
-    		scope_name = setting['scope']
-    		# scope_name.gsub! /(^|[ ])-[^ ]+/, '' # strip negated scopes
-    		# scope_name.gsub! /\./, '_' # change inner '.' to '_'
-    		# #scope_name.gsub! /(^|[ ])/, '\1.'
-    		# scope_name.gsub! /[ ]/, '_' # spaces to underscores
-    		# scope_name.gsub! /(^|,\s+)/m, '\1'
- 	      add_style_from_textmate_theme scope_name, setting['settings']
- 	    end
-  	end
+    theme_plist['settings'].each do | setting |
+      if (!setting['name'] and setting['settings'])
+        body_bg = setting['settings']['background'] || '#ffffff'
+        body_fg = setting['settings']['foreground'] || '#000000'
+        selection_bg = setting['settings']['selection']
+        body_bg = hex_color_to_rtf(body_bg)
+        body_fg = hex_color_to_rtf(body_fg)
+        selection_bg = hex_color_to_rtf(selection_bg) if selection_bg
+        @colors << body_fg
+        next
+      end
+      if setting['name'] && setting['scope']
+        scope_name = setting['scope']
+        # scope_name.gsub! /(^|[ ])-[^ ]+/, '' # strip negated scopes
+        # scope_name.gsub! /\./, '_' # change inner '.' to '_'
+        # #scope_name.gsub! /(^|[ ])/, '\1.'
+        # scope_name.gsub! /[ ]/, '_' # spaces to underscores
+        # scope_name.gsub! /(^|,\s+)/m, '\1'
+        add_style_from_textmate_theme scope_name, setting['settings']
+      end
+    end
   end
 
   def color_table
@@ -139,82 +139,82 @@ RTF_DOC
   # Search heuristic is based on the Theme Builder bundle's
   # "Create for Current Language" command
   def find_theme(uuid)
-  	theme_dirs = [
-  		File.expand_path('~/Library/Application Support/TextMate/Themes'),
-  		'/Library/Application Support/TextMate/Themes',
-  		TextMate.app_path + '/Contents/SharedSupport/Themes'
-  	]
+    theme_dirs = [
+      File.expand_path('~/Library/Application Support/TextMate/Themes'),
+      '/Library/Application Support/TextMate/Themes',
+      TextMate.app_path + '/Contents/SharedSupport/Themes'
+    ]
 
-  	theme_dirs.each do |theme_dir|
-  		if File.exists? theme_dir
-  			themes = Dir.entries(theme_dir).find_all { |theme| theme =~ /.+\.(tmTheme|plist)$/ }
-  			themes.each do |theme|
-  				plist = PropertyList.load(File.open("#{theme_dir}/#{theme}"))
-  				return plist if plist["uuid"] == uuid
-  			end
-  		end
-  	end
-  	return nil
+    theme_dirs.each do |theme_dir|
+      if File.exists? theme_dir
+        themes = Dir.entries(theme_dir).find_all { |theme| theme =~ /.+\.(tmTheme|plist)$/ }
+        themes.each do |theme|
+          plist = OSX::PropertyList.load(File.open("#{theme_dir}/#{theme}"))
+          return plist if plist["uuid"] == uuid
+        end
+      end
+    end
+    return nil
   end
   
   def detab(str, width)
-  	lines = str.split(/\n/)
-  	lines.each do | line |
-  		line_sans_markup = line.gsub(/<[^>]*>/, '').gsub(/&[^;]+;/i, '.')
-  		while (index = line_sans_markup.index("\t"))
-  			tab = line_sans_markup[0..index].jlength - 1
-  			padding = " " * ((tab / width + 1) * width - tab)
-  			line_sans_markup.sub!("\t", padding)
-  			line.sub!("\t", padding)
-  		end
-  	end
-  	return lines.join("\n")
+    lines = str.split(/\n/)
+    lines.each do | line |
+      line_sans_markup = line.gsub(/<[^>]*>/, '').gsub(/&[^;]+;/i, '.')
+      while (index = line_sans_markup.index("\t"))
+        tab = line_sans_markup[0..index].jlength - 1
+        padding = " " * ((tab / width + 1) * width - tab)
+        line_sans_markup.sub!("\t", padding)
+        line.sub!("\t", padding)
+      end
+    end
+    return lines.join("\n")
   end
   
   def document_to_rtf(input, opt = {})
-  	# Read the source document / selection
-  	# Convert tabs to spaces using configured tab width
-  	input = detab(input, (ENV['TM_TAB_SIZE'] || '8').to_i)
+    # Read the source document / selection
+    # Convert tabs to spaces using configured tab width
+    input = detab(input, (ENV['TM_TAB_SIZE'] || '8').to_i)
 
     input.gsub! /\\/, "__backslash__"
-  	input.gsub! /\\n/, "__newline__"
-  	input.gsub! /\n/, "\\\\line\n"
-  	input.gsub! /\{/, "\\{" 
-  	input.gsub! /\}/, "\\}"
-  	input.gsub! /__newline__/, "\\\\\\n"
-  	input.gsub! /__backslash__/, "\\\\\\"
+    input.gsub! /\\n/, "__newline__"
+    input.gsub! /\n/, "\\\\line\n"
+    input.gsub! /\{/, "\\{" 
+    input.gsub! /\}/, "\\}"
+    input.gsub! /__newline__/, "\\\\\\n"
+    input.gsub! /__backslash__/, "\\\\\\"
 
-  	
-  	@style_stack = []
+    
+    @style_stack = []
 
-  	# Meat. The poor-man's tokenizer. Fortunately, our input is simple
-  	# and easy to parse.
-  	tokens = input.split(/(<[^>]*>)/)
-  	code_rtf = ''
-  	tokens.each do |token|
-  		case token
-  		when /^<\//
-  		  # closing tag
-  		  pop_style
-  		when /^<>$/
-  		  # skip empty tags, resulting from name = ''
-  		when /^</
-  		  if token =~ /^<([^>]+)>$/
-  		    scope = $1
-  		    push_style scope
-  			end
-  		else
-  		  next if token.empty?
-  		  code_rtf << '{'
-  		  style = current_style_as_rtf
-  		  if style && !style.empty? && !token.strip.empty?
-  		    code_rtf << current_style_as_rtf << ' '
-		    end
-  			code_rtf << token << '}'
-  		end
-  	end
-  	
-  	return code_rtf
+    # Meat. The poor-man's tokenizer. Fortunately, our input is simple
+    # and easy to parse.
+    tokens = input.split(/(<[^>]*>)/)
+    code_rtf = ''
+    tokens.each do |token|
+      case token
+      when /^<\//
+        # closing tag
+        pop_style
+      when /^<>$/
+        # skip empty tags, resulting from name = ''
+      when /^</
+        if token =~ /^<([^>]+)>$/
+          scope = $1
+          push_style scope
+        end
+      else
+        next if token.empty?
+        code_rtf << '{'
+        style = current_style_as_rtf
+        if style && !style.empty? && !token.strip.empty?
+          code_rtf << current_style_as_rtf << ' '
+        end
+        code_rtf << token << '}'
+      end
+    end
+    
+    return code_rtf
   end
   
   def current_style
